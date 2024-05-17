7883751239d33a901bd7905b88f7f25b5cdbc1b3
geonokwon <furitstore1@naver.com>
2024-05-18 02:03:20 +0900

    🎉 [24.05.18] 자료구조 알고리즘 공부

---
+++ src/training/ch06/T_6_3_question_1.java
@@ -0,0 +1,72 @@
+package training.ch06;
+
+import java.util.Scanner;
+
+public class T_6_3_question_1 {
+    //버블정렬(버전3)
+    //a[idx1] 과 a[idx2]의 값을 교환
+    static void swap(int[] a, int idx1, int idx2){
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+    //버블정렬
+    static void bubbleSort(int[] a, int n){
+        int ccnt = 0;       //비교 횟수
+        int scnt = 0;       //교환 횟수
+        int k = 0;          //a[k] 보다 앞쪽은 정렬을 마친 상태
+        int passCount = 1;  //패스 카운터
+
+        while(k < n - 1){
+            int last = n - 1;
+            System.out.printf("패스%d : \n", passCount++);
+            int exchg = 0;                   //패스에서 교환하는 횟수 저장
+            for (int j = n - 1; j > k; j--){
+                for (int m = 0; m < n - 1; m++){
+                    System.out.printf("%3d %c", a[m], (m != j - 1) ? ' ' : (a[j - 1] > a[j]) ? '+' : '-' );
+                }
+                System.out.printf("%3d\n", a[n - 1]);
+                ccnt++;
+                if (a[j - 1] > a[j]){
+                    swap(a, j -1, j);
+                    exchg++;
+                    scnt++;
+                    last = j;
+                }
+            }
+            for (int m = 0; m < n; m++){
+                if (m == 0) {
+                    System.out.println("정렬된 값은" + "\n");
+                }
+                System.out.printf("%3d ", a[m]);
+            }
+            System.out.println();
+            k = last;
+            if (exchg == 0) break; //교환이 이루어지지 않으므로 멈춤
+        }
+        System.out.println("비교를 " + ccnt + "회 했습니다.");
+        System.out.println("교환을 " + scnt + "회 했습니다.");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("버블 정렬(버전3)");
+        System.out.print("요솟수 : ");
+        int nx = scanner.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++){
+            System.out.print("x[" + i + "] : ");
+            x[i] = scanner.nextInt();
+        }
+
+        bubbleSort(x, nx);
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++){
+            System.out.println("x[" + i + "] = " + x[i]);
+        }
+    }
+
+}
